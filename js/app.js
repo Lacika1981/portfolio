955edf89ff574f887b701cfa390b000bdcaffd09
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-09-16 21:48:13 +0100

    working on the education section, adding timeline

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-/* $(function(){
-    let widthArray = [];
-    let $width = $('.progress-bar').each(function(){
-        widthArray.push($(this).css('width'));
-    })
-    widthArray.forEach(function(widths){
-        console.log(widthArray);
-    })
-$('.progress-bar').each(function(){
-    for(let i = 0; i < widthArray.length; i++){
-    $(this).css('width', widthArray[i]);
-    }
-});
-}) */
 $(function () {
     $(".progress div").each(function () {
         var display = $(this),
