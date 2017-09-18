199b9e15649f7300fb2f79750a58e85b03e181a2
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-09-18 22:06:09 +0100

    working on the portfolio slide in effect

--- js/app.js
+++ js/app.js
@@ -14,6 +14,36 @@ $(function () {
 
 $('body').scrollspy({ target: '#navbar-example' })
 
-var test = function(){
-    console.log('hello');
-}
+/* https://www.sitepoint.com/scroll-based-animations-jquery-css3/ */
+
+$(function(){
+    var $animatedElement = $('li.animated');
+    var $window = $(window);
+
+    $window.on('scroll resize', checkInView);
+    $window.trigger('scroll');
+
+    function checkInView() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
+      
+        $.each($animatedElement, function() {
+          var $element = $(this);
+          var element_height = $element.outerHeight();
+          var element_top_position = $element.offset().top;
+          var element_bottom_position = (element_top_position + element_height);
+      
+          //check to see if this current container is within viewport
+          if ((element_bottom_position >= window_top_position) &&
+              (element_top_position <= window_bottom_position)) {
+            if ($element.hasClass('timeline-inverted')){
+                $(this).addClass('slideInRight')
+            }
+            if ($element.hasClass('timeline')){
+                $(this).addClass('slideInLeft')
+            }
+          }
+        });
+      }
+})
