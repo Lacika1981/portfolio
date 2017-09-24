d6ad395a53f875e0f0d2c42c311334dcb66194d8
Laszlo Varga <vargalaszlo1981@googlemail.com>
2017-09-24 22:08:47 +0100

    changing 'var' keywords to 'let' keywords, new function to check for space in text

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".progress div").each(function () {
-        var display = $(this),
+        let display = $(this),
             nextValue = $(this).attr("aria-valuenow");
         $(display)
             .animate({
@@ -10,98 +10,90 @@ $(function () {
                 width: 'easing'
             });
     });
-})
+});
 
-$('body').scrollspy({ target: '#navbar-example' })
+$('body').scrollspy({ target: '#navbar-example' });
 
 /* https://www.sitepoint.com/scroll-based-animations-jquery-css3/ */
 
-$(function(){
-    var $animatedElement = $('li.animated');
-    var $window = $(window);
+$(function () {
+    let $animatedElement = $('li.animated');
+    let $window = $(window);
 
     $window.on('scroll resize', checkInView);
     $window.trigger('scroll');
 
     function checkInView() {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
-      
-        $.each($animatedElement, function() {
-          var $element = $(this);
-          var element_height = $element.outerHeight();
-          var element_top_position = $element.offset().top;
-          var element_bottom_position = (element_top_position + element_height);
-      
-          //check to see if this current container is within viewport
-          if ((element_bottom_position >= window_top_position) &&
-              (element_top_position <= window_bottom_position)) {
-            if ($element.hasClass('timeline-inverted')){
-                $(this).addClass('slideInRight')
+        let window_height = $window.height();
+        let window_top_position = $window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
+
+        $.each($animatedElement, function () {
+            let $element = $(this);
+            let element_height = $element.outerHeight();
+            let element_top_position = $element.offset().top;
+            let element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                if ($element.hasClass('timeline-inverted')) {
+                    $(this).addClass('slideInRight');
+                }
+                if ($element.hasClass('timeline')) {
+                    $(this).addClass('slideInLeft');
+                }
             }
-            if ($element.hasClass('timeline')){
-                $(this).addClass('slideInLeft')
-            }
-          }
         });
-      }
-})
-
-
-/*/**
- * Vertically center Bootstrap 3 modals so they aren't always stuck at the top
- */
-
-/* https://www.abeautifulsite.net/vertically-centering-bootstrap-modals */
-
-/* $(function() {
-    function reposition() {
-        var modal = $(this),
-            dialog = modal.find('.modal-dialog');
-        modal.css('display', 'block');
-        
-        // Dividing by two centers the modal exactly, but dividing by three 
-        // or four works better for larger screens.
-        dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
     }
-    // Reposition when a modal is shown
-    $('.modal').on('show.bs.modal', reposition);
-    // Reposition when the window is resized
-    $(window).on('resize', function() {
-        $('.modal:visible').each(reposition);
-    });
-}); */
+});
 
-$(document).on('click', 'button', function(elem){
-    var clickedElem = elem.target;
-    var clickedAttribute = clickedElem.getAttribute('data-jsonTarget')
+$(document).on('click', 'button', function (elem) {
+    let clickedElem = elem.target;
+    let clickedAttribute = clickedElem.getAttribute('data-jsonTarget');
     portfolioCall(clickedAttribute);
-})
+});
 
-function portfolioCall(data){   
-        $.ajax({
-            url: 'js/portfolio.json',
-            dataType: 'json'
-        })
-        .done(function(response){
-            var objectPortfolio = response;
-            var keys = Object.keys(objectPortfolio);
-            for (let i = 0; i < keys.length; i++){
+function portfolioCall(data) {
+    $.ajax({
+        url: 'js/portfolio.json',
+        dataType: 'json'
+    })
+        .done(function (response) {
+            let objectPortfolio = response;
+            let keys = Object.keys(objectPortfolio);
+            for (let i = 0; i < keys.length; i++) {
                 objectPortfolio[keys[i]].githubImage = "../images/mark-github.svg";
                 objectPortfolio[keys[i]].linkInsideImage = "../images/kebab-horizontal.svg";
             }
 
-            var responseTitle = '<h5 class="modal-title" id="exampleModalLongTitle">%data%</h5>'.replace("%data%", response[data].title);
-            var responseBody = '<p class="card-text">%data%</p>'.replace("%data%", response[data].body);
-            var responseImage = '<img class="img-fluid" src="%data%">'.replace("%data%", response[data].image);
-            var responseLink = response[data].linkGitHub;
-            var responseGitHub = '<a href="' + responseLink + '" target="_blank"><img class="img-fluid" src="%data%"></a>'.replace("%data%", response[data].githubImage);
-            var responseLinkInside = response[data].linkInsidePage;
-            var responseLinkInsideImage = '<a href="' + responseLinkInside + '" target="_blank"><img class="img-fluid" src="%data%"></a>'.replace("%data%", response[data].linkInsideImage);
+            let responseTitle = '<h5 class="modal-title" id="exampleModalLongTitle">%data%</h5>'.replace("%data%", response[data].title);
+            let responseBody = '<p class="card-text">%data%</p>'.replace("%data%", response[data].body);
+            let responseImage = '<img class="img-fluid" src="%data%">'.replace("%data%", response[data].image);
+            let responseLink = response[data].linkGitHub;
+            let responseGitHub = '<a href="' + responseLink + '" target="_blank"><img class="img-fluid" src="%data%"></a>'.replace("%data%", response[data].githubImage);
+            let responseLinkInside = response[data].linkInsidePage;
+            let responseLinkInsideImage = '<a href="' + responseLinkInside + '" target="_blank"><img class="img-fluid" src="%data%"></a>'.replace("%data%", response[data].linkInsideImage);
             $('.modal-header').empty().append(responseTitle);
             $('.modal-body').empty().append(responseBody).append(responseImage);
             $('.gitHubIcon').empty().append(responseGitHub);
             $('.insideIcon').empty().append(responseLinkInsideImage);
         });
 }
+
+$(function(){
+    $('p.card-text').each(function(){ // take card text
+        let text = $(this)[0].innerText; // take value of card text
+        let textLength = text.length; // length of the text
+        let maxLength = 40; // max length
+
+        if (textLength > maxLength) { // checking max length
+            let a = text.charAt(maxLength); // what character at that position
+            if (a != ' '){ // check if it is a space
+                let space = text.indexOf(' ', maxLength); // check for space from the position of maxLength
+                let b = text.slice(0, space); // slice the text
+                $(this).empty().append(b + ' ...'); // empty the container and append the text
+            }
+        }
+    });
+});
